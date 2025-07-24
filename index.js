7b5be669a5ad0dfe052f5c9c9507a7731875344e
Srushtee1706 <srupatil1706@gmail.com>
2025-07-24 18:13:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const projects = [
   "Jewellery-company landing page",
   "Random Image Generator",
   "New Year Countdown",
+  "Stylish Animated loginpage",
   // Add more project names as needed
 ];
 

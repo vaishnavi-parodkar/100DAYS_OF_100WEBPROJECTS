fe2cb112243c9d84d09c8fa97b6e6a1633bc298f
Saba Fatima <xsabafatima01@gmail.com>
2025-07-26 19:22:56 +0530

    #191

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const projects = [
   "New Year Countdown",
   "Stylish Animated loginpage",
   "BMI Calculator",
-   "QR Generator",
+  "QR Generator",
   "Rock Paper Scissors Game",
-
-
-
+  "Nasa Astronomy Picture of the day",
   "Reading Journal"
-
   // Add more project names as needed
 ];
 
@@ -54,10 +51,12 @@ projects.forEach((name, index) => {
   const day = `Day ${String(index + 1).padStart(2, "0")}`;
   let link;
   if (name === "QR Generator") {
-  link = `public/Day 9/index.html`;
+    link = `public/Day 9/index.html`;
+  } else if (name === "Nasa Astronomy Picture of the day") {
+    link = "https://sabaaa01.github.io/NASA-astronomy-photo-of-the-day/";
   } else {
-  const folder = `day${String(index + 1).padStart(2, "0")}`;
-  link = `public/${folder}/index.html`;
+    const folder = `day${String(index + 1).padStart(2, "0")}`;
+    link = `public/${folder}/index.html`;
   }
 
   const row = document.createElement("tr");

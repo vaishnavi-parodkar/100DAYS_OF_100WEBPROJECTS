1c4aea2819ef4c4aa4831bc13ac2a6d20df0af12
rutikakengal <rutikakengal96@gmail.com>
2025-07-29 18:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ const projects = [
   "Colour Picker",
   "Drawing Canvas",
   "Nasa Astronomy Picture of the day",
-  "World Clock"
+  "World Clock",
   "Mood Timer",
   "text to PDF Convertor",
   "Memory Card Game",
   "Email Validator",
   "Snake And Ladder Game",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
   // Add more project names as needed
 ];
 
